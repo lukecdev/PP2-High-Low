35ab61be02365a9609c409b49441fdbf9f593071
lukecdev <courtney.luke@outlook.com>
2023-04-20 16:48:05 +0100

    Update script.js

---
+++ assests/js/script.js
@@ -0,0 +1,8 @@
+
+/** Game is to generate two seperate mountain ranges and is asking two is the first mounatin
+ * higher or lower than the second mountain. The user presses the higher button or lower button. 
+ * If the answer is correct the game continues and if incorrect the game ends.
+ */
+
+// Scope Function Start
+
