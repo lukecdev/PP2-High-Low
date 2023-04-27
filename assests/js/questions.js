d571e5ecc046c80d523f73a45d3a4673c8a6a164
lukecdev <courtney.luke@outlook.com>
2023-04-27 16:33:35 +0100

    Create questions.js

---
+++ assests/js/questions.js
@@ -0,0 +1,7 @@
+let europe =[
+    {
+        "question": "Which mountain is located in kerry and has a height of 1998mts?",
+        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
+        "correct": "Mt.Brandon"
+    }
+];
