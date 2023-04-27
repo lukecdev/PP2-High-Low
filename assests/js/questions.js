4f8a571403e66b56a3e694b73799e39f26025274
lukecdev <courtney.luke@outlook.com>
2023-04-27 17:20:30 +0100

    Update questions.js

--- assests/js/questions.js
+++ assests/js/questions.js
@@ -3,5 +3,24 @@ let europe =[
         "question": "Which mountain is located in kerry and has a height of 1998mts?",
         "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
         "correct": "Mt.Brandon"
+    },
+    {
+        "question": "Which mountain is located in kerry and has a height of 2998mts?",
+        "choices":["Mt.Brandon","Mt.Patrick","2Mt.Alps"],
+        "correct": "Mt.Patrick"
     }
 ];
+
+let world = [
+    {
+        "question": "Which mountain is located in alps and has a height of 1998mts?",
+        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
+        "correct": "Mt.Brandon"
+    },
+    {
+        "question": "Which  is located in france and has a height of 1998mts?",
+        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
+        "correct": "Mt.Brandon"
+    }
+
+]
