92bda38b5c0bfd837075cb51512330494c1a60e7
lukecdev <courtney.luke@outlook.com>
2023-04-28 14:47:34 +0100

    Update questions.js

--- assests/js/questions.js
+++ assests/js/questions.js
@@ -1,26 +1,67 @@
 let europe =[
     {
         "question": "Which mountain is Europes tallest peak and is located in the Caucasus Mountains?",
-        "choices":["Mt.Brandon","Mt.Elbrus","22Mt.Alps"],
+        "choices":["Grossglockner","Mt.Elbrus","Dufourspitze"],
         "correct": "Mt.Elbrus"
     },
     {
-        "question": "Which mountain is located in kerry and has a height of 2998mts?",
-        "choices":["Mt.Brandon","Mt.Patrick","2Mt.Alps"],
-        "correct": "Mt.Patrick"
-    }
+        "question": "Which mountain in Georgia has an elevation of 5,193m ?",
+        "choices":["Mount Kazbek","Mont Blanc","Shkhara"],
+        "correct": "Shkhara"
+    },
+    {
+        "question": "Which mountain is located in Switzerland and has an elevation of 4,634 m?",
+        "choices":["Galtymore","Dufourspitze","Carnedd Llewelyn"],
+        "correct": "Dufourspitze"
+    },
+    {
+        "question": "Which mountain is located in Austria and has an elevation of 3,798 m?",
+        "choices":["Grossglockner ","Dufourspitze","Galdhøpiggen"],
+        "correct": "Grossglockner "
+    },
+    {
+        "question": "Which mountain rises to an astonishing 4,809 m in elevation and is known as 'White Mountain'?",
+        "choices":["Helvellyn","Shkhara","Mont Blanc"],
+        "correct": "Mont Blanc"
+    },
+    {
+        "question": "Which mountain is located in Italy and has an elevation of 4,809 m?",
+        "choices":["Nephin","Monte Bianco","Mt. Aneto"],
+        "correct": "Monte Bianco"
+    },
+
 ];
 
 let world = [
     {
-        "question": "Which mountain is located in alps and has a height of 1998mts?",
-        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
-        "correct": "Mt.Brandon"
+        "question": "Which mountain is also known as Mount McKinley and is the tallest mountain in North America.?",
+        "choices":["Denali ","Monte Bianco","Mt. Aneto"],
+        "correct": "Denali"
     },
     {
-        "question": "Which  is located in france and has a height of 1998mts?",
-        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
-        "correct": "Mt.Brandon"
-    }
+        "question": "Which mountain is the second tallest mountain in the U.S. Which mountain is this?",
+        "choices":["Mount Whitney","Mount Saint Elias","Mt. Bona"],
+        "correct": "Mount Saint Elias"
+    },
+    {
+        "question": "What is the highest mountain peak in Canada with an elevation of 19,551 feet (5,959 meters)?",
+        "choices":["Mt. Hunter","Mt. Fairweather","Mt. Bona"],
+        "correct": "Mt. Logan"
+    },
+    {
+        "question": "What is the longest mountain range in the US and Canada stretching over 3,000 miles (4,800 kilometers)?",
+        "choices":["Mt. Bear","The Rocky Mountains","Laurentian Mountains"],
+        "correct": "The Rocky Mountains"
+    },
+    {
+        "question": "What is the oldest mountain range in the US and Canada?",
+        "choices":["Alaska Range","Mt. Whitney","Appalachian Mountains"],
+        "correct": "Appalachian Mountains"
+    },
+    {
+    "question": "Which mountain range extends from the Yukon to the South of British Columbia?",
+    "choices":["Coast Mountains","Teton Range","Gaspé Peninsula"],
+    "correct": "Coast Mountains"
+    },
 
 ]
