62eba8ec9821f9a6474357be11fb4e98edfc7af7
lukecdev <courtney.luke@outlook.com>
2023-04-28 13:48:48 +0100

    Update questions.js

--- assests/js/questions.js
+++ assests/js/questions.js
@@ -1,8 +1,8 @@
 let europe =[
     {
-        "question": "Which mountain is located in kerry and has a height of 1998mts?",
-        "choices":["Mt.Brandon","Mt.Patrick","22Mt.Alps"],
-        "correct": "Mt.Brandon"
+        "question": "Which mountain is Europes tallest peak and is located in the Caucasus Mountains?",
+        "choices":["Mt.Brandon","Mt.Elbrus","22Mt.Alps"],
+        "correct": "Mt.Elbrus"
     },
     {
         "question": "Which mountain is located in kerry and has a height of 2998mts?",
