d1b098dcdd5d2e58d38861d240558296094d6aa0
lukecdev <courtney.luke@outlook.com>
2023-05-05 11:28:35 +0100

    fix spelling in JS file

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,4 @@
-
-/** Game is to generate two seperate mountain ranges and is asking two is the first mounatin
- * higher or lower than the second mountain. The user presses the higher button or lower button. 
-
- */
+/* JS for quiz area */
 
 //variables
 
